904b92688de22def0bdee1ce8fdb0e82e5c4f04f
César Ivan Delgado Silva <Csar Ivan Delgado Silva>
2017-04-20 20:46:42 -0400

    changes

--- main.rb
+++ main.rb
@@ -25,13 +25,15 @@ def display
   glMatrixMode(GL_MODELVIEW)
 
   #Dibujamos un triangulo
-  glBegin(GL_TRIANGLES)
+  glBegin(GL_QUADS)
     glColor3f(1.0,0.0,0.0) #Color del primer vertice
-    glVertex3f(0.0,0.8,0.0) #coordenadas del primer vector
-    glColor3f(0.0,1.0,0.0) #Color del segundo vertice
-    glVertex3f(-0.6,-0.2,0.0) #coordenadas del segundo vector
+    glVertex3f(-0.6,0.8,0.0) #coordenadas del primer vector
+    glColor3f(0.0,1.0,1.0) #Color del segundo vertice
+    glVertex3f(0.6,0.8,0.0) #coordenadas del segundo vector
     glColor3f(0.0,0.0,1.0) #Color del tercer vertice
     glVertex3f(0.6,-0.2,0.0) #coordenadas del tercer vector
+    glColor3f(0.0,1.0,0.0) #Color del cuarto vertice
+    glVertex3f(-0.6,-0.2,0.0) #coordenadas del cuarto vector
   glEnd
   glFlush
   glutPostRedisplay
