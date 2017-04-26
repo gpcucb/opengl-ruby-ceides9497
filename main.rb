6711de6df29418a7237e63b40b2b7dba9f99cf84
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-04-25 20:25:02 -0400

    en clases modificaciones

--- main.rb
+++ main.rb
@@ -9,37 +9,48 @@ include Glut
 puts ("Hola Mundo")
 
 def display
+  #activar Buffer
+  glDepthFunc(GL_LEQUAL);
+  glEnable(GL_DEPTH_TEST);
+  glClearDepth(1.0);
+
   #Color de Fondo:negro
   glClearColor(0.0,0.0,0.0,0.0)
   #borramos la pantalla
-  glClear(GL_COLOR_BUFFER_BIT)
+  glClear(GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT)
   #modo proyeccion
   glMatrixMode(GL_PROJECTION)
   #cargamos la matriz identidad
   glLoadIdentity
 
   #Proyeccion ortografica, dentro de un cubo
-  glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0)
+  #glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0)
+gluPerspective(60.0,1.0,1.0,100.0);
+glTranslatef(0.0,0.0,-16.0);
 
   #modo de Modelado
   glMatrixMode(GL_MODELVIEW)
 
   #Dibujamos un triangulo
-  glBegin(GL_QUADS)
-    glColor3f(1.0,0.0,0.0) #Color del primer vertice
-    glVertex3f(-0.6,0.8,0.0) #coordenadas del primer vector
-    glColor3f(0.0,1.0,1.0) #Color del segundo vertice
-    glVertex3f(0.6,0.8,0.0) #coordenadas del segundo vector
-    glColor3f(0.0,0.0,1.0) #Color del tercer vertice
-    glVertex3f(0.6,-0.2,0.0) #coordenadas del tercer vector
-    glColor3f(0.0,1.0,0.0) #Color del cuarto vertice
-    glVertex3f(-0.6,-0.2,0.0) #coordenadas del cuarto vector
-  glEnd
+  glBegin(GL_QUADS); #Dibujamos un cuadrado
+    glColor3f(0.0,1.0,1.0) #Color del primer vertice
+    glVertex3f(-0.5,0.5,-0.5);#Coordenadas del primer vértice (superior-izquierda)
+    glVertex3f(-0.5,-0.5,0.5);#Coordenadas del segundo vértice (inferior-izquierda)
+    glVertex3f(0.5,-0.5,0.5);#Coordenadas del primer vértice (inferior-derecha)
+    glVertex3f(0.5,0.5,-0.5);# Coordenadas del primer vértice (superior-derecha)
+  glEnd();
+
+  glBegin(GL_TRIANGLES);
+    glColor3f(1.0,0.0,0.0);
+    glVertex3f(0.0,0.5,0.0);
+    glVertex3f(-0.7,-0.5,0.0);
+    glVertex3f(0.7,-0.5,0.0);
+  glEnd();
+
   glFlush
-  glutPostRedisplay
 end
 glutInit
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
 glutInitWindowSize(500,500)
 glutInitWindowPosition(20,20)
 glutCreateWindow("Hola OpenGL, en Ruby")
